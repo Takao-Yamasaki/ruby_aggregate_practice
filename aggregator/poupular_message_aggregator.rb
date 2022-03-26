7c61349f6d77630a950cc4aea64a7910850c25ce
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-26 20:11:34 +0900

    課題３の実装

--- aggregator/poupular_message_aggregator.rb
+++ aggregator/poupular_message_aggregator.rb
@@ -7,8 +7,24 @@ class PopularMessageAggregator
 
   # 実装してください
   def exec
-    # dummy
-    
+    result = []
+
+    @channel_names.each do |channel_name|
+      data = self.load(channel_name)
+      data["messages"].each do |m|
+        hash = {}
+        hash[:text] =  m["text"]
+        if  m["reactions"].nil?
+          next
+        else
+          m["reactions"].each do |r|
+            hash[:reaction_count] = r["count"]
+          end
+        end
+        result << hash
+      end
+    end
+    result.max {|a, b| a[:reaction_count] <=> b[:reaction_count] }
   end
 
   def load(channel_name)
